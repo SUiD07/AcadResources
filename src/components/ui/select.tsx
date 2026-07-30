555c929965ed4710c8505287adf3f3a82103cd06
BLKdrone <sasisa.blink@gmail.com>
2026-07-31 01:21:20 +0700

    fix : select item + click easier

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -66,14 +66,15 @@ function SelectContent({
       <SelectPrimitive.Content
         data-slot="select-content"
         position={position}
+        // Force style override so Radix's dynamic JS inline styles cannot exceed 300px
         style={{
           maxHeight: "300px",
           ...style,
         }}
         className={cn(
-          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-[80] max-h-(--radix-select-content-available-height) min-w-[8rem] origin-(--radix-select-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-md border shadow-md",
+          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-[9999] min-w-[8rem] overflow-hidden rounded-md border shadow-md",
           position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+            "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
           className,
         )}
         {...props}
@@ -83,7 +84,7 @@ function SelectContent({
           className={cn(
             "p-1",
             position === "popper" &&
-            "h-(--radix-select-trigger-height) w-full min-w-(--radix-select-trigger-width) scroll-my-1",
+              "w-full min-w-[var(--radix-select-trigger-width)]"
           )}
           style={{
             maxHeight: "290px",
@@ -97,6 +98,7 @@ function SelectContent({
     </SelectPrimitive.Portal>
   );
 }
+
 function SelectLabel({
   className,
   ...props
